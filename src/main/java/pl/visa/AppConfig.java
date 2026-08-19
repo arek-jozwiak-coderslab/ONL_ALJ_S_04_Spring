6fd04b153ffc28c31590bac13029f19cff4d2e09
arcyro <arek@arcyro.pl>
2026-08-19 12:29:19 +0200

    spring mvc

--- src/main/java/pl/visa/AppConfig.java
+++ src/main/java/pl/visa/AppConfig.java
@@ -3,11 +3,14 @@ package pl.visa;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import pl.visa.beans.HelloWorld;
 
 @Configuration
 @ComponentScan
-public class AppConfig {
+@EnableWebMvc
+public class AppConfig implements WebMvcConfigurer {
 
     @Bean
     public HelloWorld helloWorld() {
---
+++ src/main/java/pl/visa/HelloConfroller.java
@@ -0,0 +1,16 @@
+package pl.visa;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloConfroller {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String hello() {
+        return "hello";
+    }
+}
