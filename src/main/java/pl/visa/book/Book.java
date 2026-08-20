3f83c5b8a22ebc1ae03cf5ad007d2961ab3aac42
arcyro <arek@arcyro.pl>
2026-08-20 14:20:02 +0200

    book entity

---
+++ src/main/java/pl/visa/book/Book.java
@@ -0,0 +1,90 @@
+package pl.visa.book;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private int ratingBook;
+    private String description;
+
+    @Column(name = "created_on")
+    private LocalDateTime createdOn;
+    @Column(name = "updated_on")
+    private LocalDateTime updatedOn;
+
+    @PrePersist
+    public void prePersist() {
+        createdOn = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedOn = LocalDateTime.now();
+    }
+
+    @Transient
+    private String combinedName;
+
+    public int getRatingBook() {
+        return ratingBook;
+    }
+
+    public void setRatingBook(int rating) {
+        this.ratingBook = rating;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(LocalDateTime createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public LocalDateTime getUpdatedOn() {
+        return updatedOn;
+    }
+
+    public void setUpdatedOn(LocalDateTime updatedOn) {
+        this.updatedOn = updatedOn;
+    }
+
+    public String getCombinedName() {
+        return combinedName;
+    }
+
+    public void setCombinedName(String combinedName) {
+        this.combinedName = combinedName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
